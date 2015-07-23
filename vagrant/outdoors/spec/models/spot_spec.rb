504b6a726cdf1d69d3b17fe2893e80f866f53612
Takashi Namikata <applicationdev0@gmail.com>
2015-07-23 21:34:47 +0900

    Spot modelに5件登録されていることを確認するテストを実装

--- vagrant/outdoors/spec/models/spot_spec.rb
+++ vagrant/outdoors/spec/models/spot_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Spot, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @spots = Spot.all
+  end
+
+  it "スポット情報として5件登録されていること" do
+    @spots.size.should == 5
+  end
+
 end
