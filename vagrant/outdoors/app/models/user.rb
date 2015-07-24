a3af087bec27438a374e5c7748be0c65e2aac172
Takashi Namikata <applicationdev0@gmail.com>
2015-07-24 22:19:33 +0900

    新規会員登録時のバリデーションを実装する

--- vagrant/outdoors/app/models/user.rb
+++ vagrant/outdoors/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  validates :password, confirmation: true, length: { in: 6..24 }, if: :password
+  validates :password_confirmation, presence: true, if: :password
+
+  validates :email, presence: true, uniqueness: { case_sensitive:false }
 end
