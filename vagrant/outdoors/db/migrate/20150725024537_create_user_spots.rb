012ff8f4fe3058cd9afc7ab20fa101deff9f11de
Takashi Namikata <applicationdev0@gmail.com>
2015-07-25 11:47:33 +0900

    スポット情報を評価するUserSpot modelを作成する

---
+++ vagrant/outdoors/db/migrate/20150725024537_create_user_spots.rb
@@ -0,0 +1,11 @@
+class CreateUserSpots < ActiveRecord::Migration
+  def change
+    create_table :user_spots do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :spot, index: true, foreign_key: true
+      t.boolean :judgment
+
+      t.timestamps null: false
+    end
+  end
+end
