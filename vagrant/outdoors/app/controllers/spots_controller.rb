bbfd08c2ebef7846eff7474a078c292a6d16886f
Takashi Namikata <applicationdev0@gmail.com>
2015-07-25 21:31:45 +0900

    未評価のスポット情報を更新する処理を実装する

--- vagrant/outdoors/app/controllers/spots_controller.rb
+++ vagrant/outdoors/app/controllers/spots_controller.rb
@@ -6,7 +6,7 @@ class SpotsController < ApplicationController
     if current_user.spots.count < Spot.all.count
       redirect_to :action => "new"
     else
-      redirect :show
+      redirect_to :action => "show"
     end
   end
 
@@ -14,7 +14,19 @@ class SpotsController < ApplicationController
   end
 
   def new
+    @spot = Spot.new
+
     rated_spot_ids = current_user.spots.map(&:id)
     @unrated_spots = Spot.where.not(id: rated_spot_ids)
+  end
+
+  def create
+    judgment_ids = params[:spot_id]
+
+    judgment_ids.each do |id|
+      current_user.user_spots.create(spot_id: judgment_ids[0], judgment: true)
+    end
+
+    redirect_to spots_url, notice: "登録が完了しました。"
   end
 end
