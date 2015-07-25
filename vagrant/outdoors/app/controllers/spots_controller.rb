4ff06d4cae6402ccbf8e5959871a9864562fa6c5
Takashi Namikata <applicationdev0@gmail.com>
2015-07-25 17:15:35 +0900

    spots_controllerの各メソッドの役割を整理する

--- vagrant/outdoors/app/controllers/spots_controller.rb
+++ vagrant/outdoors/app/controllers/spots_controller.rb
@@ -2,20 +2,19 @@ class SpotsController < ApplicationController
   before_filter :require_login
 
   def index
-    @user = current_user
-
-    #評価済みのスポット情報を取得
-    @rated_spots = @user.spots
-
-    #全て評価済みか判定
-    if @rated_spots.count < Spot.all.count
-      rated_spot_ids = @rated_spots.map(&:id)
-      @unrated_spots = Spot.where.not(id: rated_spot_ids)
+    #評価済みのスポット情報を取得し、全て評価済みか判定
+    if current_user.spots.count < Spot.all.count
+      redirect_to :action => "new"
     else
-      #showページにリダイレクトする処理を実装する
+      redirect :show
     end
   end
 
   def show
+  end
+
+  def new
+    rated_spot_ids = current_user.spots.map(&:id)
+    @unrated_spots = Spot.where.not(id: rated_spot_ids)
   end
 end
