02aefc72d3033393768353dbdca2f90830987b3a
Takashi Namikata <applicationdev0@gmail.com>
2015-07-25 15:47:45 +0900

    中間テーブルに登録されていないスポット情報を取得し、画面に表示する

--- vagrant/outdoors/app/controllers/spots_controller.rb
+++ vagrant/outdoors/app/controllers/spots_controller.rb
@@ -2,6 +2,20 @@ class SpotsController < ApplicationController
   before_filter :require_login
 
   def index
-    @spots = Spot.all
+    @user = current_user
+
+    #評価済みのスポット情報を取得
+    @rated_spots = @user.spots
+
+    #全て評価済みか判定
+    if @rated_spots.count < Spot.all.count
+      rated_spot_ids = @rated_spots.map(&:id)
+      @unrated_spots = Spot.where.not(id: rated_spot_ids)
+    else
+      #showページにリダイレクトする処理を実装する
+    end
+  end
+
+  def show
   end
 end
