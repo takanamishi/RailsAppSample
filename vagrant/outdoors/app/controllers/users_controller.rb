9e897b5c2029db783929fa57000953956b57df38
Takashi Namikata <applicationdev0@gmail.com>
2015-08-01 21:49:59 +0900

    退会処理の動線を設ける

--- vagrant/outdoors/app/controllers/users_controller.rb
+++ vagrant/outdoors/app/controllers/users_controller.rb
@@ -50,10 +50,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_url, notice: "退会が完了しました。"
   end
 
   private
